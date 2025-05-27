ec9457b6490485d45a6fbfd20bfc702b673dcf37
SeMyeong Park <myeong@gupsa.com>
2025-05-27 10:13:30 +0900

    useImpreativeHandle로 컴포넌트API노출

--- src/features/minigame/ui/ResultModal.jsx
+++ src/features/minigame/ui/ResultModal.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useImperativeHandle, useRef } from "react";
 
 function ResultModal({ ref, result, targetTime, styles }) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  }); //ref객체와 다른 컴포넌트에 메서드나 프로퍼티 등 노출해야하는 내용을 그룹화하는 객체를 반환하는 함수를 인수로 받음
+
   return (
-    <dialog ref={ref} className={styles['result-modal']}>
+    <dialog ref={dialog} className={styles["result-modal"]}>
       <h2>You {result}</h2>
       <p>
         The target time was <srtong>{targetTime} seconds.</srtong>
       </p>
-      <p>You stopped the timer with <strong>X seconds left.</strong></p>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>
+      </p>
       <form method="dialog">
         <button>Close</button>
       </form>
--- src/widgets/TimeChallenge/TimeChallenge.jsx
+++ src/widgets/TimeChallenge/TimeChallenge.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import ResultModal from "../../features/minigame/ui/ResultModal";
 
-
 function TimeChallenge({ title, targetTime, styles }) {
   const timer = useRef();
   const dialog = useRef();
@@ -12,8 +11,7 @@ function TimeChallenge({ title, targetTime, styles }) {
   const handleStart = () => {
     timer.current = setTimeout(() => {
       setTimeExpired(true);
-      dialog.current.showModal();
-
+      dialog.current.open();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -25,18 +23,18 @@ function TimeChallenge({ title, targetTime, styles }) {
 
   return (
     <>
-    {timerStared && <ResultModal ref={dialog} targetTime={targetTime} result="lost" styles={styles} />}
-    <section className={styles.challenge}>
-      <h2>{title}</h2>
-      {timeExpired && <p>you lost</p>}
-      <p className={styles["challenge-time"]}>
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={timerStared ? handleStop : handleStart}>{timerStared ? "Stop" : "Start"} Challenge</button>
-      </p>
-      <p className={timeExpired ? "active" : null}>{timerStared ? "Time is running" : "Time inactive"}</p>
-    </section>
+      {timerStared && <ResultModal ref={dialog} targetTime={targetTime} result="lost" styles={styles} />}
+      <section className={styles.challenge}>
+        <h2>{title}</h2>
+        {timeExpired && <p>you lost</p>}
+        <p className={styles["challenge-time"]}>
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStared ? handleStop : handleStart}>{timerStared ? "Stop" : "Start"} Challenge</button>
+        </p>
+        <p className={timeExpired ? "active" : null}>{timerStared ? "Time is running" : "Time inactive"}</p>
+      </section>
     </>
   );
 }

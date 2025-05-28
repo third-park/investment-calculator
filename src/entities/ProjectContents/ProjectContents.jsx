2e36f099ee964daae01a59af25db7ef45c94e731
SeMyeong Park <myeong@gupsa.com>
2025-05-28 14:13:38 +0900

    project save시 데이터흐름 표시

--- src/entities/ProjectContents/ProjectContents.jsx
+++ src/entities/ProjectContents/ProjectContents.jsx
@@ -1,7 +1,28 @@
 import Input from "../../shared/Input.jsx";
-import React from "react";
+import React, { useRef } from "react";
 
-function ProjectContents() {
+
+/** 프로젝트 add 폼 */
+function ProjectContents({ onAdd }) {
+  //1. useRef로 input 참조 생성
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  const handleSave = () => {
+    //2. save버튼 클릭시 ref.current통해서 사용자 입력값 받아오기
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    //3. 저장할 객체를 onAdd(=handleAddProject 함수)를 통해 보내기(MgmProjects로 데이터 전달)
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -9,16 +30,15 @@ function ProjectContents() {
           <button className="text-stone-800 hover:text-stone-950">Cancel</button>
         </li>
         <li>
-          <button className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+          <button className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>Save</button>
         </li>
       </menu>
       <div className="">
-        <Input label="title" id="title" type="text" />
-        <Input label="description" id="description" textarea />
-        <Input label="dueDate" id="dueDate" type="date" />
+        <Input ref={title} label="title" id="title" type="text" />
+        <Input ref={description} label="description" id="description" textarea />
+        <Input ref={dueDate} label="dueDate" id="dueDate" type="date" />
       </div>
     </div>
-    // 생성한 project 클릭되면 템플릿 생성 + task input 생성해서 내용 입력시 해당 내용 저장(창을 바꿔도 계속 저장되어야함)
   );
 }
 
--- src/pages/MgmProjects/MgmProjects.jsx
+++ src/pages/MgmProjects/MgmProjects.jsx
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../../entities/Sidebar/Sidebar";
 import ProjectContents from "../../entities/ProjectContents/ProjectContents";
 import NoProjectSelected from "../../shared/NoProjectSelected";
 
-
 function MgmProjects() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  //ProjectContents컴포넌트(자식컴포넌트)에서 함수실행할때 전달한 객체(인수)를 부모컴포넌트에서 파라미터로 사용
+  const handleAddProject = (projectData) => {
+    //4. 프로젝트 객체 생성, id 추가
+    const newProject = {
+      ...projectData,
+      id: Math.random(),
+    };
+
+    //5. 기존값에 새 객체추가해서 상태 업데이트
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  };
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <ProjectContents onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <>
       <section className="w-screen my-8 h-screen flex gap-8 bg-white pt-20">
-        <Sidebar />
-        {/* <ProjectContents /> */}
-        <NoProjectSelected />        
+        <Sidebar onStartAddProject={handleStartAddProject} />
+        {content}
       </section>
     </>
   );
